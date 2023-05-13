a9b4884393d1c072f5f04597d86f4922d523bb55
Tahera29 <120545807+Tahera29@users.noreply.github.com>
2023-05-13 16:23:48 +0300

    script.js

--- script.js
+++ script.js
@@ -8,20 +8,46 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  
+    arr.map(student)=> {
+    if(student.marks>50)
+    {console.log(student);}
+  }
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  
+  arr.forEach(student)=> {
+    if(student.marks>50)
+    {console.log(student);}
+  }
 }
 
 function addData() {
   //Write your code here, just console.log
+  
+  const newstudent({id:4,name:"susan",age:"20",marks:45};)
+  arr.push(newstudent);
+  console.log(newstudent);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  
+  const failedstudents= arr.filter(student)=>{
+  return (student.marks<50)
+  }
+  console.log(failedstudents);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  
+   let newarr = [
+    { id: 4, name: "sam", age: "18", marks: 80 },
+    { id: 5, name: "jackie", age: "17", marks: 85 },
+    { id: 6, name: "lauren", age: "19", marks: 35 },
+  ];
+  arr.concat(newarr);
 }
